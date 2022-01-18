640e0420bcf6e948b59c63d61d903d6ccbb7804e
AnthonyQuiranza <anthony.quiranza@upec.edu.ec>
2022-01-18 10:20:35 -0500

    First commit

---
+++ src/scripts/animation.js
@@ -0,0 +1,16 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    cursorChar: '💓',
+    typeSpeed: 40,
+    backDelay: 3000,
+
+
+  });
+
+  var typedTitle = new Typed('#typedTitle', {
+    stringsElement: '#typedTitleString',
+    loop: true,
+    typeSpeed: 70,
+  });
+
+  new GreenAudioPlayer('.reproductor');
